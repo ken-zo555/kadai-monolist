b6e8c59b28bcdbeb10a028ee7f49d4e660d6b5ff
ken-zo555 <ken_zo555@yahoo.co.jp>
2018-01-08 12:20:38 +0000

    have

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :items, through: :ownerships
   has_many :wants
   has_many :want_items, through: :wants, class_name: 'Item', source: :item
+  has_many :haves, class_name: 'Have'
+  has_many :have_items, through: :haves, class_name: 'Item', source: :item
   
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -17,10 +19,24 @@ class User < ApplicationRecord
   
   def unwant(item)
     want = self.wants.find_by(item_id: item.id)
+    want.destroy if want
   end
   
   def want?(item)
     self.want_items.include?(item)
+  end
+  
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+  
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id)
+    have.destroy if have
+  end
+  
+  def have?(item)
+    self.have_items.include?(item)
   end
 
 end
